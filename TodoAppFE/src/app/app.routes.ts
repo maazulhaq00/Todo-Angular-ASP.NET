b137a6dc4069a90d56943d86b0f7db712cd59d24
maaz <maazulhaq00@gmail.com>
2024-10-19 18:53:47 +0500

    component & route

--- TodoAppFE/src/app/app.routes.ts
+++ TodoAppFE/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { TodoComponent } from './components/todo/todo.component';
 
 export const routes: Routes = [
-    
+    {
+        path: "",
+        component: TodoComponent
+    }
 ];
